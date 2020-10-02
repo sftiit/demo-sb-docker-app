a6e398a2fb8e98843cf485af75ee2d43172eac32
sftiit <61592891+sftiit@users.noreply.github.com>
2020-10-01 23:18:34 -0400

    Update AppControllerTest.java
    Read data from a file

--- SpringBoot/src/test/java/com/leidos/app/AppControllerTest.java
+++ SpringBoot/src/test/java/com/leidos/app/AppControllerTest.java
@@ -2,6 +2,10 @@ package com.leidos.app;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,12 +13,12 @@ import javax.persistence.Id;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
 import com.leidos.app.entity.AppEntity;
 import com.leidos.app.repository.AppRepository;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -31,16 +35,39 @@ public class AppControllerTest {
 	@Test
 	@Rollback(false)
 	public void createRow() {
-		AppEntity appEntity = appRepository.save(new AppEntity(id, "cloud4", "sekar4", "sekar4@gmail.com", true));
-		assertThat(appEntity.getId());
+		String row = readTestFile();
+		if (row != null) 
+		{
+			String[] items = row.split(",");
+			AppEntity appEntity = appRepository.save(new AppEntity(id, items[0].toString(), items[1].toString(), 
+					items[2].toString(), Integer.parseInt(items[3].toString()) == 1 ? true : false));
+			assertThat(appEntity.getId());
+		}
 	}
 	
 	
 	@Test
 	public void getAValue() {
-		AppEntity appEntity = appRepository.getOne(new Long(100));
-		assertThat(appEntity.getMember().equalsIgnoreCase("sekar1"));
+		AppEntity appEntity = appRepository.getOne(new Long(1000));
+		assertThat(appEntity.getMember().equalsIgnoreCase("sekar"));
 	}
 	
+	public String readTestFile() {
+		String fileName = "testdata.txt";
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new InputStreamReader(
+                    this.getClass().getResourceAsStream("/" + fileName)));
+			String line = reader.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
